a1949d54fe5a5e4c88880977d7d218a68af3d6d5
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2022-10-31 00:46:50 +0200

    redesign architecture

--- src/backend/api/bind.hpp
+++ src/backend/api/bind.hpp
@@ -1,27 +1,25 @@
 #pragma once
 
 #include "webview.h"
-#include "json.hpp"
 
 #include "window/window.hpp"
 #include "filesystem/fs.hpp"
 
 extern webview::webview w;
-using json = nlohmann::json;
 
 namespace api {
 
 
     void BindInit() noexcept {
 
-        w.bind("api_setWindowSize", api::SetWindowSize);
-        w.bind("api_setWindowTitle", api::SetWindowTitle);
-        w.bind("api_closeWindow", api::CloseWindow);
-        w.bind("api_makeDir", api::MakeDirectory);
-        w.bind("api_readFile", api::ReadFile);
-        w.bind("api_writeFile", api::WriteFile);
-        w.bind("api_appendFile", api::AppendFile);
-        w.bind("api_removeFile", api::RemoveFile);
+        w.bind("win_setWindowSize", api::SetWindowSize);
+        w.bind("win_setWindowTitle", api::SetWindowTitle);
+        w.bind("win_closeWindow", api::CloseWindow);
+        w.bind("fs_makeDir", api::MakeDirectory);
+        w.bind("fs_readFile", api::ReadFile);
+        w.bind("fs_writeFile", api::WriteFile);
+        w.bind("fs_appendFile", api::AppendFile);
+        w.bind("fs_removeFile", api::RemoveFile);
     }
 
 
