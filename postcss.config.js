3c8df8b83aa1b77a948475989b6c0c46b4c5d3eb
cjnix35 <doge.perseverance@gmail.com>
2022-11-04 17:47:54 +0200

    remove old files

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require("autoprefixer");
-const tailwindcss = require("tailwindcss");
-module.exports = {
-  plugins: [
-    "postcss-preset-env",
-    tailwindcss("./tailwind.config.js"),
-    autoprefixer,
-  ],
-};
