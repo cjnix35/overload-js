672b52abcf0f6100bc6871520cef7523770c304a
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2022-11-21 01:09:09 +0200

    add header with app info

---
+++ src/backend/def.hpp
@@ -0,0 +1,8 @@
+#define APP_NAME "overload.js"
+#define APP_INTERNAL_NAME "overload-js"
+#define WIN_TITLE "overload.js"
+#define VERSION "v1.0"
+#define LICENSE "MIT"
+#define REFERENCE "https://github.com/cjnix35/overload-js"
+// If you want to compile production build, change 'true' on 'false'
+#define DEBUG true
