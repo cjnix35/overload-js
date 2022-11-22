629a3cd1ed35248005bb3bae6b782f8b2cb8b371
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2022-11-22 20:55:20 +0200

    add new includes to filesystem header

--- src/backend/api/filesystem/fs.hpp
+++ src/backend/api/filesystem/fs.hpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <fstream>
 #include <sstream>
+#include <cstdlib>
 
 #include "../rapidjson/document.h"
 #include "../rapidjson/writer.h"
@@ -10,10 +11,28 @@
 
 #include "../misc.hpp"
 
+#include "../../def.hpp"
+
+#include <archive.h>
+#include <archive_entry.h>
+
+#ifdef __linux__ //|| (__APPLE__ && __MACH__)
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#elif _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#include <direct.h>
+#define chdir _chdir
+
+#endif
+
 using namespace rapidjson;
+using Resource = std::string;
 
 namespace api {
-
 
     std::string MakeDir(std::string args);
     std::string ReadFile(std::string args);
@@ -23,6 +42,15 @@ namespace api {
     std::string RemoveDir(std::string args);
     std::string ListDir(std::string args);
     std::string AbsolutePath(std::string args);
+
+    static int copy_data(struct archive *ar, struct archive *aw);
+    static int extract_file(const char *filename);
+    static int extract_mem(std::uint8_t *buf, std::uint64_t buf_len);
+
+    bool LoadResourcesFromFile(std::string filename);
+    bool LoadResourcesFromMemory(std::uint8_t *buf, std::uint64_t *buf_len);
+    std::string ResourcePath(std::string res_name);
+    Resource GetResourceContent(std::string res_name);
 
 
 }; // namespace api
