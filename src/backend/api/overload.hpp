78ae673e346dda073129d540ee27ec742eef3c55
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2022-11-23 23:52:04 +0200

    update header

--- src/backend/api/overload.hpp
+++ src/backend/api/overload.hpp
@@ -1,23 +1,48 @@
 #pragma once
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+#ifndef WEBVIEW_H
 #include "webview.h"
+#define WEBVIEW_H
+#endif
+
 #include "../def.hpp"
 #include "misc.hpp"
+#include "filesystem/fs.hpp"
+#include "sys/os/os.hpp"
+#include "sys/hardware/cpu.hpp"
+#include "sys/hardware/display.hpp"
+#include "sys/hardware/mem.hpp"
+
+using namespace rapidjson;
 
 namespace api {
 
-    using binding_t = std::function<void(std::string, std::string, void*)>;
+    using binding_t = std::function<void(std::string, std::string, void *)>;
+    using sync_binding_t = std::function<std::string(std::string)>;
 
     class OverApp {
 
 
+        private:
+            void bind_init() noexcept;
+            std::string SetWindowSize(std::string args);
+            std::string SetWindowTitle(std::string args);
+            std::string CloseWindow(std::string args) noexcept;
+            std::string AsyncCPP(std::string args);
+            webview::webview w;
+
+
         public:
-            template <typename pointer>
-            OverApp::OverApp(bool debug = false, pointer window = nullptr);
-            ~OverApp::OverApp();
+            OverApp(bool debug = false, void *winptr = nullptr)
+                : w(debug, winptr) {
+            }
             void run();
 
-            void bind(std::string func_name, binding_t func);
+            void bind(const std::string &func_name, sync_binding_t &func);
             void unbind(std::string func_name);
             void set_title(std::string title) noexcept;
             void set_size(std::uint64_t x, std::uint64_t y) noexcept;
@@ -27,7 +52,7 @@ namespace api {
             void eval(const std::string js);
             void init(const std::string js);
             void terminate() noexcept;
-            void* window();
+            void *window();
             void dispatch(std::function<void()> f);
 
             bool set_signal(std::string signal, std::function<void()> f);
@@ -35,16 +60,6 @@ namespace api {
             std::string set_signal(std::string args);
             // For JS
             // List of all possible signals below
-
-
-        private:
-            bool is_debug;
-            void* winptr;
-            webview::webview w(debug, window);
-            constexpr char* ApplicationData = [
-                APP_NAME, APP_INTERNAL_NAME, VERSION, LICENSE, REFERENCE,
-                DEBUG
-            ];
     };
 
 
